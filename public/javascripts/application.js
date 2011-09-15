31c8f731d11c02d7670160d284d064f098f4aad8
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-09-14 22:20:33 -0500

    mostrar promesas

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,17 @@
 $.extend({
- 
+ show_milestone_comments : function() {
+   $('.hito_header').click(function(){
+     visible = $('.hito_content:visible').attr('id');
+     $('.hito_content').slideUp(600);
+     number = $(this).attr('id').substr(12)
+     content =  $('#hito_content_' + number);
+     if (visible != content.attr('id')) {
+       content.slideDown(600);
+     }
+   })
+ }
 });
 
-$(document).ready(function(){  
+$(document).ready(function(){
+  $.show_milestone_comments();
 });
