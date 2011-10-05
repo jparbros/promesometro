4a2307725661b1cf077ee90917ac74e9d0e459c4
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-10-05 08:27:05 -0500

    assets para mejorar el desempeño

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,8 @@ Promesometro::Application.configure do
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
+  
+  config.action_controller.asset_host = "http://asset%d.promesometro.pe"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
