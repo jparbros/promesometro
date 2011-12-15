bf1233645108152b95acdbf37ca5270e84ae290c
Jorge Pardiñas <jorge.parbros@crowdint.com>
2011-12-15 05:00:45 -0600

    mejorando para postgres

--- app/models/promise_search.rb
+++ app/models/promise_search.rb
@@ -22,26 +22,26 @@ class PromiseSearch
   end
   
   def find_by_title title
-    @promise_search = @promise_search.where('promises.title LIKE ?',"%#{title}%") if title
+    @promise_search = @promise_search.where('promises.title iLIKE ?',"%#{title}%") if title
   end
   
   def find_by_state state_name
-    @promise_search = @promise_search.joins(:official => :state).where('states.name LIKE ?',"%#{state_name}%") if state_name
+    @promise_search = @promise_search.joins(:official => :state).where('states.name iLIKE ?',"%#{state_name}%") if state_name
   end
   
   def find_by_province province_name
-    @promise_search = @promise_search.joins(:official => :province).where('provinces.name LIKE ?',"%#{province_name}%") if province_name
+    @promise_search = @promise_search.joins(:official => :province).where('provinces.name iLIKE ?',"%#{province_name}%") if province_name
   end
   
   def find_by_official official_name
-    @promise_search = @promise_search.joins(:official).where('officials.name LIKE ?',"%#{official_name}%") if official_name
+    @promise_search = @promise_search.joins(:official).where('officials.name iLIKE ?',"%#{official_name}%") if official_name
   end
   
   def find_by_keywords keywords
-    @promise_search = @promise_search.where('title LIKE ? OR description LIKE ?',"%#{keywords}%", "%#{keywords}%") if keywords
+    @promise_search = @promise_search.where('title iLIKE ? OR description iLIKE ?',"%#{keywords}%", "%#{keywords}%") if keywords
   end
   
   def find_by_topic topic
-    @promise_search = @promise_search.joins(:topics).where('topics.name LIKE ?',"%#{topic}%") if topic
+    @promise_search = @promise_search.joins(:topics).where('topics.name iLIKE ?',"%#{topic}%") if topic
   end
 end
